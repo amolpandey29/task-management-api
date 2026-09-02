d7d82d7402f1a11df24439a9c6102a3460acb025
Amol Pandey <amolpandey33@gmail.com>
2026-09-02 15:44:00 +0530

    Add MySQL setup script

---
+++ MySQL_DB_Creation.sql
@@ -0,0 +1,23 @@
+-- CREATE DATABASE task_management;
+
+-- SHOW DATABASES;
+
+-- USE task_management;
+
+-- CREATE TABLE tasks (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     title VARCHAR(255) NOT NULL,
+--     completed BOOLEAN DEFAULT FALSE
+-- );
+
+-- INSERT INTO tasks(title, completed) VALUES ("Learn FastAPI", FALSE);
+
+-- SELECT * FROM tasks;
+
+-- UPDATE tasks SET completed = TRUE WHERE id = 1;
+
+-- SELECT * FROM tasks;
+
+-- DELETE FROM tasks WHERE id = 1;
+
+SELECT * FROM tasks;
