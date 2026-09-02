7c2800b389dd946aea8e4f23c73de89f27870c11
Amol Pandey <amolpandey33@gmail.com>
2026-09-02 14:34:58 +0530

    Add task update endpoint

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ class TaskCreate(BaseModel):
 	title: str
 	completed: bool = False
 
+class TaskUpdate(BaseModel):
+	title: str | None = None
+	completed: bool | None = None
+
 tasks = [
 	{
 		"id": 1,
@@ -43,4 +47,15 @@ def get_task(task_id: int):
 	for task in tasks:
 		if task["id"] == task_id:
 			return task
-	raise HTTPException(status_code=404, detail="Task not found") 
+	raise HTTPException(status_code=404, detail="Task not found")
+
+@app.patch("/tasks/{task_id}")
+def update_task(task_id: int, task: TaskUpdate):
+	for existing_task in tasks:
+		if existing_task["id"] == task_id:
+			if task.title is not None:
+				existing_task["title"] = task.title
+			if task.completed is not None:
+				existing_task["completed"] = task.completed
+			return existing_task
+	raise HTTPException(status_code=404,detail="Task not found")
